61abe839245f7b61b6763b0b33c241126fdaf505
mic georgin <georgin.aymeric@gmail.com>
2015-04-06 16:32:38 +0200

    Ajoute les tests pour le changement de title pour les pages home, contact et about

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,23 +4,44 @@ RSpec.describe PagesController, type: :controller do
   render_views
 
   describe "GET #home" do
-    it "returns http success" do
+    before do
       get :home
+    end
+
+    it "returns http success" do
       expect(response).to have_http_status(:success)
+    end
+
+    it "doit avoir dans son title (HTML) le mot accueil" do
+      expect(response.body).to have_title("Simple App du Tutoriel Ruby on Rails | Accueil")
     end
   end
 
   describe "GET #contact" do
-    it "returns http success" do
+    before do
       get :contact
+    end
+
+    it "returns http success" do
       expect(response).to have_http_status(:success)
+    end
+
+    it "doit avoir dans son title (HTML) le mot accueil" do
+      expect(response.body).to have_title("Simple App du Tutoriel Ruby on Rails | Accueil")
     end
   end
 
   describe "GET #about" do
-    it "returns http success" do
+    before do
       get :about
+    end
+
+    it "returns http success" do
       expect(response).to have_http_status(:success)
+    end
+
+    it "doit avoir dans son title (HTML) le mot accueil" do
+      expect(response.body).to have_title("Simple App du Tutoriel Ruby on Rails | Accueil")
     end
   end
 
