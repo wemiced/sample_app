478ac338026434ebd431f4767069b6930b4d3427
mic georgin <georgin.aymeric@gmail.com>
2015-04-06 18:05:45 +0200

    Ajoute le test pour la page Pages#help

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe PagesController, type: :controller do
   render_views
 
+  before do
+    @base_titre = "Simple App du Tutoriel Ruby on Rails |"
+  end
+
   describe "GET #home" do
     before do
       get :home
@@ -13,7 +17,7 @@ RSpec.describe PagesController, type: :controller do
     end
 
     it "doit avoir dans son title (HTML) le mot accueil" do
-      expect(response.body).to have_title("Accueil")
+      expect(response.body).to have_title("#{@base_titre} Accueil")
     end
   end
 
@@ -26,8 +30,8 @@ RSpec.describe PagesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
 
-    it "doit avoir dans son title (HTML) le mot accueil" do
-      expect(response.body).to have_title("Contact")
+    it "doit avoir dans son title (HTML) le mot contact" do
+      expect(response.body).to have_title("#{@base_titre} Contact")
     end
   end
 
@@ -40,8 +44,22 @@ RSpec.describe PagesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
 
-    it "doit avoir dans son title (HTML) le mot accueil" do
-      expect(response.body).to have_title("About")
+    it "doit avoir dans son title (HTML) le mot about" do
+      expect(response.body).to have_title("#{@base_titre} About")
+    end
+  end
+
+  describe "GET #help" do
+    before do
+      get :help
+    end
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "doit avoir dans son title (HTML) le mot help" do
+      expect(response.body).to have_title("#{@base_titre} Help")
     end
   end
 
