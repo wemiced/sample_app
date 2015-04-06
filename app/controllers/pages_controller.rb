bad9497095ea38bdd5e0f0e41932c1c057d22636
mic georgin <georgin.aymeric@gmail.com>
2015-04-06 16:52:42 +0200

    Le titre change dynamiquement en fonction des pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @title = "Accueil"
   end
 
   def contact
+    @title = "Contact"
   end
 
   def about
+    @title = "About"
   end
 end
