248d202a46223626c0d37614ac22ac298f4e29fa
mic georgin <georgin.aymeric@gmail.com>
2015-04-06 15:47:11 +0200

    Ajoute l'action about au controller pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
+  render_views
 
   describe "GET #home" do
     it "returns http success" do
@@ -12,6 +13,13 @@ RSpec.describe PagesController, type: :controller do
   describe "GET #contact" do
     it "returns http success" do
       get :contact
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
       expect(response).to have_http_status(:success)
     end
   end
