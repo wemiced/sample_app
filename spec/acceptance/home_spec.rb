24628344d86064e61ba466f37e1e391331a76ae2
mic georgin <georgin.aymeric@gmail.com>
2015-04-07 22:34:27 +0200

    Ajoute une spec vérifiant les liens sur la home

---
+++ spec/acceptance/home_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+feature "Home" do
+  scenario "has links to other pages" do
+    visit root_path
+    expect(page).to have_link('About', href: about_path)
+    expect(page).to have_link('Home', href: home_path)
+    expect(page).to have_link('Contact', href: contact_path)
+    expect(page).to have_link('Help', href: help_path)
+  end
+end
