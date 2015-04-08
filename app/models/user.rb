f590a3a922a371d610afe4d10a1902b8aaa9dc83
mic georgin <georgin.aymeric@gmail.com>
2015-04-08 19:07:58 +0200

    Deplace la ligne validates :password, length minimum 6 en dessous de la ligne has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-
-  validates :password, length: { minimum: 6 }
   has_secure_password
+  validates :password, length: { minimum: 6 }
 end
