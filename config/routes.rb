5f35c15784810af6b04128dfb65a49a31b96bf9b
mic georgin <georgin.aymeric@gmail.com>
2015-04-09 09:44:06 +0200

    Ajoute la route users_show

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'pages/about', as: "about"
   get 'pages/help', as: "help"
   get 'users/new', as: "signup"
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
